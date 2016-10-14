477c0922becd67bd3e4139757c5b1ced3cfc6076
js345 <1994yxf@gmail.com>
2016-10-13 22:28:08 -0500

    code done

--- PLSA.py
+++ PLSA.py
@@ -6,6 +6,7 @@ Created on 10/13/16
 
 from __future__ import division
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class PLSA:
@@ -27,16 +28,109 @@ class PLSA:
         self.docs, self.word_count_list, self.word_dict = self.read_file(filename)
         # random initialization of parameters
         self.pi_s = [np.random.dirichlet(np.ones(K)) for i in range(len(self.docs))]
-        self.theta_s = [np.random.dirichlet(np.ones(len(self.word_dict))) for i in range(K)]
+        # K dictionaries of values sum to 1 in each
+        self.theta_s = [{k: v for k, v in zip(self.word_dict,np.random.dirichlet(np.ones(len(self.word_dict))))} for i in range(K)]
         # generate background model
         total = sum(self.word_dict.values())
         self.word_dict = {k: v / total for k, v in self.word_dict.iteritems()}
+        # log likelihood
+        self.log_p = None
 
     def e_step(self):
-        pass
+        # np 2d array ndk dim |D| * K
+        n_dk = np.zeros((len(self.docs), self.K))
+        # nwk dim |V| * K dict of 1d array
+        n_wk = {word: np.zeros(self.K) for word in self.word_dict}
+        # estimate ndk
+        for i in range(len(self.docs)):
+            for k in range(self.K):
+                n_dk[i][k] = self.calculate_ndk(i, k)
+        # estimate nwk
+        for word in self.word_dict:
+            for k in range(self.K):
+                n_wk[word][k] = self.calculate_nwk(word, k)
 
-    def m_step(self):
-        pass
+        return n_dk, n_wk
+
+    def m_step(self, n_dk, n_wk):
+        # update pi
+        for i in range(len(self.docs)):
+            for k in range(self.K):
+                self.pi_s[i][k] = n_dk[i][k] / sum(n_dk[i])
+        # update theta
+        for k in range(self.K):
+            for word in self.theta_s[k]:
+                self.theta_s[k][word] = n_wk[word][k] / sum([n_wk[word][k] for word in n_wk])
+
+    def run(self, iteration=100, diff=0.0001):
+        self.log_p = self.compute_log()
+        log_graph, log_diff_graph = list(), list()
+        for i in range(iteration):
+            n_dk, n_wk = self.e_step()
+            self.m_step(n_dk, n_wk)
+            log_p = self.compute_log()
+            log_diff = log_p - self.log_p
+            log_graph.append(log_p)
+            log_diff_graph.append(log_diff)
+            if log_diff < diff:
+                break
+        return log_graph, log_diff_graph
+
+    def compute_log(self):
+        """
+        Compute log likelihood
+        :return:
+        :rtype:
+        """
+        log_likelihood = 0
+        for i in range(len(self.docs)):
+            for j in range(len(self.docs[i])):
+                inner_sum = 0
+                for k in range(self.K):
+                    inner_sum += self.pi_s[i][k] * self.theta_s[k][self.docs[i][j]]
+                inner_sum = inner_sum * (1 - self.lamb) + self.lamb * self.word_dict[self.docs[i][j]]
+                log_likelihood += inner_sum
+        return log_likelihood
+
+    def calculate_ndk(self, i, k):
+        """
+        Calculate ndk given document i and topic k
+        :param i:
+        :type i:
+        :param k:
+        :type k:
+        :return:
+        :rtype:
+        """
+        ndk = 0
+        for word in self.word_count_list[i]:
+            p_sum = 0
+            for k_p in range(self.K):
+                p_sum += self.pi_s[i][k_p] * self.theta_s[k_p][word]
+            denominator = self.lamb * self.word_dict[word] + (1 - self.lamb) * p_sum
+            nominator = (1 - self.lamb) * self.pi_s[i][k] * self.theta_s[k][word]
+            ndk += self.word_count_list[i][word] * nominator / denominator
+        return ndk
+
+    def calculate_nwk(self, word, k):
+        """
+        Calculate ndk given word and topic k
+        :param word:
+        :type word:
+        :param k:
+        :type k:
+        :return:
+        :rtype:
+        """
+        nwk = 0
+        for i in range(len(self.word_count_list)):
+            p_sum = 0
+            for k_p in range(self.K):
+                p_sum += self.pi_s[i][k_p] * self.theta_s[k_p][word]
+            denominator = self.lamb * self.word_dict[word] + (1 - self.lamb) * p_sum
+            nominator = (1 - self.lamb) * self.pi_s[i][k] * self.theta_s[k][word]
+            nwk += self.word_count_list[i][word] * nominator / denominator
+        return nwk
 
     @staticmethod
     def read_file(filename):
@@ -63,3 +157,10 @@ class PLSA:
 
 if __name__ == '__main__':
     plsa = PLSA()
+    log_graph, diff_graph = plsa.run()
+    plt.plot(log_graph)
+    plt.title("Log likelihood plot")
+    plt.show()
+    plt.plot(diff_graph)
+    plt.title("Log differnece plot")
+    plt.show()
